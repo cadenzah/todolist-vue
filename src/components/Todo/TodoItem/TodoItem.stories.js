14354cf8047634f0597d1cb2d25bde3cf859afdc
cadenzah <cadenzah93@gmail.com>
2020-12-11 01:06:29 +0900

    added storybook story

--- src/components/Todo/TodoItem/TodoItem.stories.js
+++ src/components/Todo/TodoItem/TodoItem.stories.js
@@ -1,6 +1,20 @@
 import TodoItem from '../TodoItem';
 import { action } from '@storybook/addon-actions';
 
+// dummy data
+const items = [
+  {
+    id: Date.now(),
+    status: 'PENDING',
+    desc: '운동하기',
+  },
+  {
+    id: Date.now() + 30,
+    status: 'DONE',
+    desc: '휴식',
+  },
+];
+ 
 export default {
   title: 'TodoItem',
   component: TodoItem,
@@ -14,8 +28,20 @@ export const actionsData = {
 
 const Template = (args, { argTypes }) => ({
   components: { TodoItem },
-  template: '<TodoItem />'
+  props: Object.keys(argTypes),
+  methods: {},
+  template:
+    `<TodoItem
+      v-bind="$props"
+    />`,
 });
 
-export const Default = Template.bind({});
-export const Confirm = Template.bind({});
+export const Pending = Template.bind({});
+Pending.args = {
+  item: items[0],
+}
+
+export const Done = Template.bind({});
+Done.args = {
+  item: items[1],
+}
