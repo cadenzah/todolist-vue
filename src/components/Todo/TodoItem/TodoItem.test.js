a2bda6404acb0e447cd24e428b9069f6ab44a62d
cadenzah <cadenzah93@gmail.com>
2020-12-10 23:54:56 +0900

    test code refactor - added beforeEach()

--- src/components/Todo/TodoItem/TodoItem.test.js
+++ src/components/Todo/TodoItem/TodoItem.test.js
@@ -1,18 +1,20 @@
 import { mount } from '@vue/test-utils';
 import TodoItem from './TodoItem.vue';
 
+let wrapper = null;
+
 describe(`TodoItem.vue`, () => {
   describe(`# 체크박스`, () => {
-    it(`Wrapper 역할의 <div>`, () => {
-      const wrapper = mount(TodoItem);
+    beforeEach(() => {
+      wrapper = mount(TodoItem);
+    });
 
+    it(`Wrapper 역할의 <div>`, () => {
       // 컨텐츠 영역을 감싸는 <div>가 존재하며, "todoitem-checkbox__wrapper" class를 가진다
       expect(wrapper.find('div.todoitem-checkbox__wrapper').exists()).toBe(true);
     });
 
     it(`체크박스를 구성하는 요소들`, () => {
-      const wrapper = mount(TodoItem);
-
       // 체크박스를 감싸는 <label>이 래퍼 <div> 내에 존재
       expect(wrapper.find('div.todoitem-checkbox__wrapper > label').exists()).toBe(true);
 
@@ -27,15 +29,11 @@ describe(`TodoItem.vue`, () => {
   describe(`# Contents`, () => {
     
     it(`Wrapper 역할의 <div>`, () => {
-      const wrapper = mount(TodoItem);
-
       // 컨텐츠 영역을 감싸는 <div>가 존재하며, "todoitem-content__wrapper" class를 가진다`
       expect(wrapper.find('div.todoitem-content__wrapper').exists()).toBe(true);
     });
 
     it(`컨텐츠가 표시되는 <span>`, () => {
-      const wrapper = mount(TodoItem);
-
       // <div class="todoitem-content__wrapper"> 내에 존재
       expect(wrapper.find('.todoitem-content--wrapper > span').exists()).toBe(true);
       // "todoitem-content__content" class를 가진다
@@ -45,8 +43,6 @@ describe(`TodoItem.vue`, () => {
 
   describe(`# Vue Component`, () => {
     it(`props`, () => {
-      const wrapper = mount(TodoItem);
-
       // props로 할일 항목 처리에 필요한 것들을 모두 전달받았는지
       const { item, handleUpdateTodo, handleDeleteTodo } = wrapper.props;
       expect(typeof item).toBe('object');
