84974a32de4f5ba016717bc362c7846e0bd3c1f5
cadenzah <cadenzah93@gmail.com>
2020-12-14 23:48:26 +0900

    added style for <TodoList />'s empty message

--- src/components/common/TodoList/TodoList.vue
+++ src/components/common/TodoList/TodoList.vue
@@ -57,5 +57,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../../scss/_variables.scss";
 
+.todolist-message__wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & > p {
+    font-size: $fontsize-todoitem;
+    color: $color-placeholder;
+  }
+}
 </style>
