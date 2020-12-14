4582883855d5bfae5f01e08b85929e624b5599b6
cadenzah <cadenzah93@gmail.com>
2020-12-15 00:00:03 +0900

    deleted unused vue files and restructured directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-import Root from './Root.vue';
-import Test from './Test.vue';
+import Home from './Home.vue';
 import NotFound from './NotFound.vue';
 
 export {
-  Root,
-  Test,
+  Home,
   NotFound,
 };
