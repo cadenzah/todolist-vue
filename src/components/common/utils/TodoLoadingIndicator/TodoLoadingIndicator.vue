12c8ce7cb441c69a65487dfd235a53d96dc95425
cadenzah <cadenzah93@gmail.com>
2020-12-19 01:38:44 +0900

    implemented <TodoLoadingIndicator />

--- src/components/common/utils/TodoLoadingIndicator/TodoLoadingIndicator.vue
+++ src/components/common/utils/TodoLoadingIndicator/TodoLoadingIndicator.vue
@@ -1,10 +1,33 @@
 <template>
-  
+  <div class="todo-loading-indicator__wrapper">
+    <div
+      v-for="item in items"
+      :key="item.id"
+    >
+      <label>
+        <input />
+        <span />
+      </label>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+const desc = 'If you have something to do, don\'t be lazy!';
 
+export default {
+  name: "TodoLoadingIndicator",
+  props: {
+    lines: {
+      type: Number,
+      default: () => (3),
+    },
+  },
+  computed: {
+    items: function() {
+      return new Array(this.lines).fill('').map((el, id) => ({ id, desc }));
+    },
+  }
 }
 </script>
 
