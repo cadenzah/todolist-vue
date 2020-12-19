f758395f0a66f6d503a2376147db7e1d69068700
cadenzah <cadenzah93@gmail.com>
2020-12-20 01:56:58 +0900

    deleted unnecessary test codes

--- src/components/common/TodoInput/TodoInput.test.js
+++ src/components/common/TodoInput/TodoInput.test.js
@@ -67,40 +67,4 @@ describe(`<TodoInput />`, () => {
       wrapper = null;
     });
   });
-
-  describe(`# Button`, () => {
-    beforeEach(() => {
-      const spyChangeInput = sinon.spy();
-      const spyCreateTodo = sinon.spy();
-      
-      wrapper = mount(TodoInput, {
-        propsData: {
-          todoDesc: "",
-          handleChangeInput: spyChangeInput,
-          handleCreateTodo: spyCreateTodo,
-        },
-      });
-    });
-    
-    it(`<Button /> 요소를 가진다`, () => {
-      // 컨텐츠 영역을 감싸는 <div>가 존재하며, "todoitem-content__wrapper" class를 가진다
-      expect(wrapper.find('div.todoitem-content__wrapper').exists()).toBe(true);
-    });
-
-    it(`버튼 색상 스타일이 primary이다`, () => {
-      expect(wrapper.find('.todoitem-content__wrapper > span').text()).toMatch('운동하기');
-
-      
-    });
-
-    it(`버튼 클릭시 이벤트 핸들러가 작동한다`, () => {
-      // 클릭 이벤트가 발생하였을 때, 현재 todoDesc의 값을 전달
-      await wrapper.find('button').trigger('click');
-      expect(spy.called).toBeTruthy();
-    });
-
-    afterEach(() => {
-      wrapper = null;
-    });
-  })
 });
